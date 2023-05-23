29135fe20af60902a1e30fbf7b0b549d775c5667
lana <diegogarciaq98@gmail.com>
2023-05-23 17:24:37 -0500

    Add rest-client gem and generate usersController to avoid CORS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
+require 'rest-client'
+
 class UsersController < ApplicationController
+  before_action :set_access_control_headers
+
+  def show
+    username = params[:username]
+    url = "https://torre.bio/api/bios/#{username}"
+    response = RestClient.get(url)
+    render json: response.body
+  end
+
+  private
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+  end
 end
