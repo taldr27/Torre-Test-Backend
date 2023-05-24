7da232ae5742f50ae88333559b09b945e9b83bd5
lana <diegogarciaq98@gmail.com>
2023-05-24 12:15:22 -0500

    Fix typo with env variable

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < ApplicationController
 
   def show
     jobid = params[:jobid]
-    url = url = ENV['TORRE_API_JOB_URL'] + jobid
+    url = url = ENV['TORRE_API_JOBS_URL'] + jobid
     response = RestClient.get(url)
     render json: response.body
   end
